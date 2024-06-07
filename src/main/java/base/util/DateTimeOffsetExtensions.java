eb52de24762d92472354dd01fa53b9f3b1bf7173
thoser666 <thoser666@web.de>
2024-06-07 16:27:14 +0200

    Initialimport

---
+++ src/main/java/base/util/DateTimeOffsetExtensions.java
@@ -0,0 +1,32 @@
+package base.util;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class DateTimeOffsetExtensions {
+    public static OffsetDateTime fromUTCUnixTimeMilliseconds(long milliseconds) {
+        return OffsetDateTime.ofInstant(
+                java.time.Instant.ofEpochMilli(milliseconds),
+                ZoneOffset.ofTotalSeconds(OffsetDateTime.now().getOffset().getTotalSeconds())
+        );
+    }
+
+    public static OffsetDateTime fromUTCUnixTimeSeconds(long seconds) {
+        return OffsetDateTime.ofInstant(
+                java.time.Instant.ofEpochSecond(seconds),
+                ZoneOffset.ofTotalSeconds(OffsetDateTime.now().getOffset().getTotalSeconds())
+        );
+    }
+
+    public static OffsetDateTime fromUTCISO8601String(String dateTime) {
+        return OffsetDateTime.parse(dateTime).withOffsetSameInstant(ZoneOffset.ofTotalSeconds(OffsetDateTime.now().getOffset().getTotalSeconds()));
+    }
+
+    public static String toUTCISO8601String(OffsetDateTime dateTime) {
+        return dateTime.withOffsetSameInstant(ZoneOffset.UTC).toString();
+    }
+
+    public static String toRFC3339String(OffsetDateTime dateTime) {
+        return toUTCISO8601String(dateTime) + "Z";
+    }
+}
