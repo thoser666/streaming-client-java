5d028ddc2d5c533c6f7e6bf11a2dc6a4534feca1
thoser666 <thoser666@web.de>
2024-06-08 10:23:44 +0200

    Class done

---
+++ src/main/java/base/util/DictionaryExtensions.java
@@ -0,0 +1,12 @@
+package base.util;
+
+import java.util.Map;
+
+public class DictionaryExtensions {
+    public static <K, V> V getOrDefault(Map<K, V> dictionary, K key) {
+        if (dictionary.containsKey(key)) {
+            return dictionary.get(key);
+        }
+        return null; // Java hat keinen direkten Äquivalent zum "default(V)" in C#
+    }
+}
