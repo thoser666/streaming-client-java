e9a1a7c57abbdf45a66b1342fc63ce4172cf8f0b
thoser666 <thoser666@web.de>
2024-06-08 10:33:08 +0200

    Tests added

--- src/test/java/base/util/DictionaryExtensionsTest.java
+++ src/test/java/base/util/DictionaryExtensionsTest.java
@@ -1,7 +1,24 @@
 package base.util;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DictionaryExtensionsTest {
+    @Test
+    public void test_returns_value_when_key_exists_in_dictionary() {
+        Map<String, String> dictionary = Map.of("key1", "value1", "key2", "value2");
+        String result = DictionaryExtensions.getOrDefault(dictionary, "key1");
+        assertEquals("value1", result);
+    }
+
+    @Test
+    public void test_handles_empty_dictionary_input() {
+        Map<String, String> dictionary = Map.of();
+        String result = DictionaryExtensions.getOrDefault(dictionary, "key1");
+        assertNull(result);
+    }
 
 }
