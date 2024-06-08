ef9abb6422b9b815f589e6efcedf4be86bf2c086
thoser666 <thoser666@web.de>
2024-06-08 10:35:14 +0200

    public removed

--- src/test/java/base/util/DictionaryExtensionsTest.java
+++ src/test/java/base/util/DictionaryExtensionsTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DictionaryExtensionsTest {
     @Test
-    public void test_returns_value_when_key_exists_in_dictionary() {
+    void test_returns_value_when_key_exists_in_dictionary() {
         Map<String, String> dictionary = Map.of("key1", "value1", "key2", "value2");
         String result = DictionaryExtensions.getOrDefault(dictionary, "key1");
         assertEquals("value1", result);
     }
 
     @Test
-    public void test_handles_empty_dictionary_input() {
+    void test_handles_empty_dictionary_input() {
         Map<String, String> dictionary = Map.of();
         String result = DictionaryExtensions.getOrDefault(dictionary, "key1");
         assertNull(result);
